82511b42eea49df968b4b2ed4464a0c11296c34d
chijiokeprince92 <chijioke.nwaiwu92@gmail.com>
2022-04-19 19:24:34 +0400

    first Commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+          <Header/>
+          <Routes>
+            <Route path="/" exact element={<ProductListing/>} />
+            <Route path="/product/:productId" exact element={<ProductDetails/>}/>
+            <Route>404 Not Found</Route>
+          </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
